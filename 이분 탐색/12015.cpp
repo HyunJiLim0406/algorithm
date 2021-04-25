a28afaec9ffd2f1d3239f932a7cbb541c923ab70
iw0406 <hyunjilim98@gmail.com>
2021-04-25 13:35:37 +0900

    재채점으로 인한 수정

--- 이분 탐색/12015.cpp
+++ 이분 탐색/12015.cpp
@@ -1,39 +1,40 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-vector<int> arr;
+int arr[1000001], dp[1000001];
 
-void binaryInsert(int input) { //lower bound
-    int left = 0;
-    int right = arr.size() - 1;
-
-    if (input > arr[right]) {
-        arr.push_back(input);
-        return;
-    }
+int lowerBound(int left, int right, int key) { //lower bound를 찾는 함수
     while (left <= right) {
         int mid = (left + right) / 2;
-        if (input <= arr[mid]) //왼쪽 탐색
-            right = mid - 1;
-        else if (input > arr[mid]) //오른쪽 탐색
+        if (dp[mid] < key)
             left = mid + 1;
+        else
+            right = mid - 1;
     }
-    //값을 replace 하는 과정, right+1이 lower bound
-    arr.erase(arr.begin() + right + 1);
-    arr.insert(arr.begin() + right + 1, input);
-    return;
+    return right + 1;
+}
+
+int lis(int n) {
+    int length = 0;
+
+    for (int i = 1; i <= n; i++) {
+        int pos = lowerBound(1, length, arr[i]); //arr[i]이 들어가야 할 위치 반환
+        dp[pos] = arr[i];
+        if (pos > length) //증가 수열의 길이가 증가하는 상황
+            length++;
+    }
+    return length;
 }
 
 int main() {
-    int N, input;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    arr.push_back(0); //초기값으로 제일 작은 0 투입
+    int N;
+
     cin >> N;
-    for (int i = 0; i < N; i++) {
-        cin >> input;
-        binaryInsert(input);
-    }
-    cout << arr.size() - 1; //0을 미리 넣어 놓았으니까 1 빼야 함
+    for (int i = 1; i <= N; i++)
+        cin >> arr[i];
+    cout << lis(N) << '\n'; //길이 출력
 }
