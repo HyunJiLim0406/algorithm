64e0f3f0777f2f4b6bc6c1528958215c0658ad4c
iw0406 <hyunjilim98@gmail.com>
2021-04-28 18:46:22 +0900

    알고리즘 기초 2/2(601 - 그래프 1 (연습))

---
+++ DFS와 BFS/16929.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <string>
+
+using namespace std;
+
+vector<vector<pair<char, bool>>> board; //문자, 방문여부
+int N, M;
+bool isCycle;
+pair<int, int> dir[4] = {{-1, 0},  //상
+                         {1,  0},  //하
+                         {0,  -1}, //좌
+                         {0,  1}}; //우
+
+void dfs(pair<int, int> cur, pair<int, int> source) { //cur=이번에 탐색할 좌표, source=cur의 이전 좌표
+    int row = cur.first;
+    int col = cur.second;
+
+    if (board[row][col].second) { //이미 방문했던 좌표면 사이클이 형성된 것
+        isCycle = true;
+        return;
+    }
+    board[row][col].second = true; //방문처리
+    for (int i = 0; i < 4 && !isCycle; i++) { //상하좌우 확인
+        int nr = row + dir[i].first;
+        int nc = col + dir[i].second;
+        if ((nr >= 0) && (nr < N) && (nc >= 0) && (nc < M) && //범위 체크
+            (board[row][col].first == board[nr][nc].first) && //같은 문자 확인
+            ((nr != source.first) || (nc != source.second))) { //source 좌표가 아닌지 확인
+            dfs(make_pair(nr, nc), cur);
+        }
+    }
+}
+
+int main() {
+    string input;
+
+    cin >> N >> M;
+    board.assign(N, vector<pair<char, bool>>(M));
+    for (int i = 0; i < N; i++) {
+        cin >> input;
+        for (int j = 0; j < M; j++) {
+            board[i][j].first = input[j];
+            board[i][j].second = false;
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (!board[i][j].second) { //미방문 좌표
+                dfs(make_pair(i, j), make_pair(-1, -1));
+                if (isCycle) { //사이클 있음
+                    cout << "Yes\n";
+                    return 0;
+                }
+            }
+        }
+    }
+    cout << "No\n"; //사이클 없음
+}
---
+++ DFS와 BFS/16947.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<vector<int>> graph;
+vector<int> status;
+vector<bool> cycle;
+bool isCycle, isFound;
+
+void dfs(int start, int cur, int source) { //시작 정점, 현재 정점, 직전 정점
+    if (status[cur]) { //이미 방문한 정점을 갔는데
+        if (cur == start) //시작 정점이면 사이클
+            isCycle = true;
+        isFound = true; //사이클이 아님
+        return;
+    }
+    status[cur] = 1; //방문 처리
+    for (int i = 0; i < graph[cur].size() && !isFound; i++) {
+        if (graph[cur][i] != source) { //직전 정점이 아니면 연결된 정점 탐색
+            dfs(start, graph[cur][i], cur);
+        }
+    }
+}
+
+int bfs(int start) {
+    queue<int> q;
+
+    status[start] = 1;
+    q.push(start);
+    while (!q.empty()) {
+        int cur = q.front(); //현재 정점
+        q.pop();
+        if (cycle[cur]) //사이클인 정점을 만나면 리턴
+            return status[cur] - 1;
+        for (int i = 0; i < graph[cur].size(); i++) {
+            if (!status[graph[cur][i]]) { //미방문 정점
+                status[graph[cur][i]] = status[cur] + 1;
+                q.push(graph[cur][i]);
+            }
+        }
+    }
+    return 0;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N, first, second;
+
+    cin >> N;
+    graph.assign(N + 1, vector<int>(0));
+    cycle.assign(N + 1, false);
+    for (int i = 0; i < N; i++) {
+        cin >> first >> second;
+        graph[first].push_back(second);
+        graph[second].push_back(first);
+    }
+
+    for (int i = 1; i <= N; i++) { //모든 정점에 대해 사이클에 포함된 정점이 맞는지 체크
+        status.assign(N + 1, 0);
+        isCycle = isFound = false;
+        dfs(i, i, -1);
+        if (isCycle)
+            cycle[i] = true;
+    }
+
+    for (int i = 1; i <= N; i++) { //모든 정점에 대해 사이클이면 0 출력, 아니면 bfs
+        if (cycle[i])
+            cout << 0 << ' ';
+        else {
+            status.assign(N + 1, 0);
+            cout << bfs(i) << ' ';
+        }
+    }
+}
