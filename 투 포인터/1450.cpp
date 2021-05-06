7ca40efe93ba97df1e4c20dfcdfd06116aa82b9f
iw0406 <hyunjilim98@gmail.com>
2021-05-06 20:31:06 +0900

    meet in the middle

--- 투 포인터/1450.cpp
+++ 투 포인터/1450.cpp
@@ -7,7 +7,7 @@ typedef long long ll;
 
 vector<ll> arr;
 
-vector<ll> splitArr(int left, int len) {
+vector<ll> splitArr(int left, int len) { //비트 마스킹으로 각 배열에 대한 연산
     vector<ll> result;
     for (int i = 0; i < (1 << len); i++) {
         ll sum = 0;
@@ -29,15 +29,14 @@ int main() {
     arr.assign(N, 0);
     for (int i = 0; i < N; i++)
         cin >> arr[i];
-    vector<ll> front_arr = splitArr(0, N / 2);
-    vector<ll> back_arr;
-    if (N % 2 == 0)
+    vector<ll> front_arr = splitArr(0, N / 2); //앞의 절반
+    vector<ll> back_arr; //뒤의 절반
+    if (N % 2 == 0) //배열의 크기가 짝수일 때
         back_arr = splitArr(N / 2, N / 2);
-    else
+    else //배열의 크기가 홀수일 때
         back_arr = splitArr(N / 2, N / 2 + 1);
-    sort(back_arr.begin(), back_arr.end());
-    for (int i = 0; i < front_arr.size(); i++) {
+    sort(back_arr.begin(), back_arr.end()); //정렬
+    for (int i = 0; i < front_arr.size(); i++) //back_arr에서 C-front_arr[i]에 대한 upper_bound 찾기
         result += upper_bound(back_arr.begin(), back_arr.end(), C - front_arr[i]) - back_arr.begin();
-    }
     cout << result;
 }
