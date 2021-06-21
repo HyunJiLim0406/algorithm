851d5140901d3675a971eb15837900811a57a1fb
iw0406 <hyunjilim98@gmail.com>
2021-06-21 13:19:53 +0900

    bfs

--- DFS와 BFS/5567.cpp
+++ DFS와 BFS/5567.cpp
@@ -17,13 +17,13 @@ int bfs(int start, int n) {
         int cur = q.front();
         q.pop();
 
-        if (dist[cur] == 2)
+        if (dist[cur] == 2) //더이상 탐색할 필요 없음
             continue;
         for (int i = 0; i < graph[cur].size(); i++) {
-            if (dist[graph[cur][i]] == 3) {
+            if (dist[graph[cur][i]] == 3) { //미방문 정점
                 dist[graph[cur][i]] = dist[cur] + 1;
                 q.push(graph[cur][i]);
-                cnt++;
+                cnt++; //초대할 수 있는 친구 수 증가
             }
         }
     }
@@ -35,7 +35,7 @@ int main() {
 
     cin >> n >> m;
     graph.assign(n + 1, vector<int>(0));
-    while (m--) {
+    while (m--) { //양방향 그래프
         cin >> a >> b;
         graph[a].push_back(b);
         graph[b].push_back(a);
