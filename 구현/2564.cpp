ba6f74731f676af4e395369e87b30f947123a76d
iw0406 <hyunjilim98@gmail.com>
2021-07-29 13:56:30 +0900

    구현 - 경비원

--- 구현/2564.cpp
+++ 구현/2564.cpp
@@ -4,42 +4,25 @@
 using namespace std;
 
 int main() {
-    int N, M, num, result = 0;
+    int N, M, num, p, d, result = 0;
 
     cin >> N >> M >> num;
     int tot_len = 2 * (N + M);
-    vector<pair<int, int>> arr(num);
-    pair<int, int> pos;
-    for (int i = 0; i < num; i++)
-        cin >> arr[i].first >> arr[i].second;
-    cin >> pos.first >> pos.second;
-
-    for (int i = 0; i < num; i++) {
-        if (arr[i].first == pos.first) //같은 구역
-            result += abs(arr[i].second - pos.second);
-        else if ((arr[i].first - 1) / 2 == (pos.first - 1) / 2) { //ㄷ자 모양
-            int tmp = arr[i].second + pos.second;
-            if ((pos.first - 1) / 2 == 0)
-                tmp += M;
-            else
-                tmp += N;
-            result += min(tmp, tot_len - tmp);
-        } else { //ㄱ자 모양
-            if (arr[i].first == 1 && pos.first == 3 || arr[i].first == 3 && pos.first == 1)
-                result += (arr[i].second + pos.second);
-            else if (arr[i].first == 1 && pos.first == 4)
-                result += (N - arr[i].second + pos.second);
-            else if (arr[i].first == 4 && pos.first == 1)
-                result += (N - pos.second + arr[i].second);
-            else if (arr[i].first == 4 & pos.first == 2)
-                result += (N - pos.second + M - arr[i].second);
-            else if (arr[i].first == 2 && pos.first == 4)
-                result += (M - pos.second + N - arr[i].second);
-            else if (arr[i].first == 2 && pos.first == 3)
-                result += (arr[i].second + M - pos.second);
-            else if (arr[i].first == 3 && pos.first == 2)
-                result += (M - arr[i].second + pos.second);
-        }
+    vector<int> dist(num + 1);
+    for (int i = 0; i <= num; i++) { //왼쪽 위 점으로부터의 거리
+        cin >> p >> d;
+        if (p == 1)
+            dist[i] = d;
+        if (p == 2)
+            dist[i] = N + M + (N - d);
+        if (p == 3)
+            dist[i] = tot_len - d;
+        if (p == 4)
+            dist[i] = N + d;
     }
-    cout<<result;
+    for (int i = 0; i < num; i++) {
+        int tmp = abs(dist[i] - dist[num]);
+        result += min(tmp, tot_len - tmp);
+    }
+    cout << result;
 }
