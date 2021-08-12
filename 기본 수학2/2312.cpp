22e9b2b20cc036e855deb02b1adc998e241db8fa
iw0406 <hyunjilim98@gmail.com>
2021-08-12 12:55:22 +0900

    수학 - 수 복원하기

--- 기본 수학2/2312.cpp
+++ 기본 수학2/2312.cpp
@@ -6,13 +6,13 @@ using namespace std;
 vector<pair<int, int>> factorization(int N) {
     vector<pair<int, int>> result;
     int mod = 2;
-    while (N != 1) {
+    while (N != 1) { //1이 되면 break
         int cnt = 0;
-        while (N % mod == 0) {
+        while (N % mod == 0) { //mod로 나눠 떨어지지 않을 때까지 나누기
             N /= mod;
             cnt++;
         }
-        if (cnt)
+        if (cnt) //한번이라도 나뉜 적 있다면
             result.emplace_back(mod, cnt);
         mod++;
     }
