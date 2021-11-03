f4d76050d417d62fa4eb3b171d6eca152dc77f16
iw0406 <hyunjilim98@gmail.com>
2021-11-03 12:26:25 +0900

    플로이드-워셜(역추적) - 플로이드 2(리팩토링)

--- 최단 경로/11780.cpp
+++ 최단 경로/11780.cpp
@@ -6,34 +6,30 @@
 using namespace std;
 const int INF = 1e9;
 
-vector<vector<int>> dist, before;
+vector<vector<int>> dist, table;
 
 void floydWarshall(int v) { //플로이드-워셜
     for (int i = 1; i <= v; i++) {
         for (int j = 1; j <= v; j++) {
             for (int k = 1; k <= v; k++) {
-                if (j == k)
+                if (j == k) //자신과의 거리
                     continue;
                 if (dist[j][k] > (dist[j][i] + dist[i][k])) {
                     dist[j][k] = dist[j][i] + dist[i][k];
-                    before[j][k] = i; //이전 정점 갱신
+                    table[j][k] = table[j][i];
                 }
             }
         }
     }
 }
 
-vector<int> v;
-void findRoute(int start, int end) { //왜 string으로 안될까??
-    if (before[start][end] == start){ //재귀 종료
-        v.push_back(start);
-        v.push_back(end);
-        return;
+vector<int> findRoute(int s, int d) {
+    vector<int> ans;
+    while (table[s][d] != d) {
+        ans.push_back(table[s][d]);
+        s = table[s][d];
     }
-    int next_node = before[start][end];
-    findRoute(start, next_node); //이전
-    v.pop_back();
-    findRoute(next_node, end); //이후
+    return ans;
 }
 
 int main() {
@@ -44,11 +40,14 @@ int main() {
 
     cin >> n >> m;
     dist.assign(n + 1, vector<int>(n + 1, INF));
-    before.assign(n + 1, vector<int>(n + 1, -1));
+    table.assign(n + 1, vector<int>(n + 1, INF));
+
     for (int i = 0; i < m; i++) {
         cin >> a >> b >> c;
-        dist[a][b] = min(dist[a][b], c);
-        before[a][b] = a;
+        if (c < dist[a][b]) {
+            dist[a][b] = c;
+            table[a][b] = b;
+        }
     }
     floydWarshall(n);
     for (int i = 1; i <= n; i++) { //최단 경로 출력
@@ -56,18 +55,18 @@ int main() {
             cout << ((dist[i][j] == INF) ? 0 : dist[i][j]) << ' ';
         cout << '\n';
     }
+
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
-            if (dist[i][j] == INF) //갈 수 없음
-                cout << 0;
-            else { //경로 출력
-                v.clear();
-                findRoute(i, j);
-                cout<<v.size()<<' ';
-                for(int k=0;k<v.size();k++)
-                    cout<<v[k]<<' ';
+            if (dist[i][j] == INF) {
+                cout << 0 << '\n';
+                continue;
             }
-            cout<<'\n';
+            vector<int> route = findRoute(i, j);
+            cout << route.size() + 2 << ' ' << i << ' ';
+            for (int k = 0; k < route.size(); k++)
+                cout << route[k] << ' ';
+            cout << j << '\n';
         }
     }
 }
