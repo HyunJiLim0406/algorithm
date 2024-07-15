fb5c6deb30b1c83d63c7fbadea01491c2af303db
HyunJi / Rain <hyunjilim98@gmail.com>
2024-07-16 08:35:19 +0900

    Time: 6355 ms (5.19%), Space: 26 MB (52.27%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
@@ -1,51 +1,31 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def buildTree(self, descriptions: List[List[int]]):
-        tree = {} #key: node, value: {left, right, parent}
-        for parent, child, isLeft in descriptions:
-            if parent not in tree:
-                tree[parent] = [None, None, None]
-            tree[parent][isLeft ^ 1] = child
-            if child in tree:
-                tree[child][2] = parent
-            else:
-                tree[child] = [None, None, parent]
-        return tree
-
-    def findRoot(self, start: int, tree: {}):
-        root = start
-        while(tree[root][2]):
-            root = tree[root][2]
-        return root
-    
-    def getTreeNode(self, root: int, tree: {}):
-        result = TreeNode(root)
-        queue = [(result, root)]
-
-        while(len(queue) != 0):
-            node, idx = queue.pop(0)            
-            treeValue = tree[node.val]
-            
-            if(treeValue[0]):
-                node.left = TreeNode(treeValue[0])
-                queue.append((node.left, treeValue[0]))
-            else:
-                node.left = None
-
-            if(treeValue[1]):
-                node.right = TreeNode(treeValue[1])
-                queue.append((node.right, treeValue[1]))
-            else:
-                node.right = None
-        return result
+    def findRoot(self, hasParent: List[int], roots: List[int]):
+        for root in roots:
+            if root not in hasParent:
+                return root
 
     def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
-        tree = self.buildTree(descriptions)
-        return self.getTreeNode(self.findRoot(descriptions[0][0], tree), tree)
-        
+        tree = {}
+        hasParent = []
+
+        for parent, child, isLeft in descriptions:
+            hasParent.append(child)
+            
+            if (parent not in tree):
+                tree[parent] = TreeNode(parent)
+            if (child not in tree):
+                tree[child] = TreeNode(child)
+            
+            if (isLeft == 1):
+                tree[parent].left = tree[child]
+            else:
+                tree[parent].right = tree[child]
+
+        root = self.findRoot(hasParent, list(map(lambda x: x[0], descriptions)))
+        return tree[root]
