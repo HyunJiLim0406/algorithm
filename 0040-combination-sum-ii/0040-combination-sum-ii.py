d1e8b6ca608778e015846688ca5357c76fc6172d
HyunJi / Rain <hyunjilim98@gmail.com>
2024-08-16 17:55:54 +0900

    Time: 122 ms (5.09%), Space: 16.6 MB (68.14%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,19 +1,19 @@
 class Solution:
     def __init__(self):
-        self.result = set()
+        self.result = []
 
     def backtracking(self, target: int, candidates: List[int], arr: List[int], index: int):
         if sum(arr) == target:
-            self.result.add(tuple(arr))
+            self.result.append(arr)
             return
         if sum(arr) > target or index == len(candidates) or sum(arr) + sum(candidates[index:]) < target:
             return
         
         nextArr = arr.copy()
         nextArr.append(candidates[index])
-        self.backtracking(target, candidates, nextArr, index + 1)
-        
         index += 1
+        self.backtracking(target, candidates, nextArr, index)
+        
         while index < len(candidates) and nextArr[-1] == candidates[index]:
             index += 1
         self.backtracking(target, candidates, arr, index)
@@ -21,4 +21,3 @@ class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         self.backtracking(target, sorted(candidates), [], 0)
         return self.result
-        
