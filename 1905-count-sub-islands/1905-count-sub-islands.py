a6f25fc6ecdcd0d3bd7f5cb4b53050e45da83937
HyunJi / Rain <hyunjilim98@gmail.com>
2024-08-29 08:13:30 +0900

    Time: 4067 ms (5.04%), Space: 36.9 MB (57.19%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -6,37 +6,35 @@ class Solution:
                 if grid[row][col] == 1:
                     candidates.append([row, col])
         return candidates
-    
-    def isValidPos(self, row, col, rowLen, colLen):
-        return 0 <= row < rowLen and 0 <= col < colLen
 
-    def bfs(self, grid1: List[List[int]], grid2: List[List[int]]):
-        result = 0
+    def bfs(self, row: int, col: int, grid1: List[List[int]], grid2: List[List[int]]):
         dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+        grid2[row][col] = 0
+        queue = [[row, col]]
+        isValid = True
+        while len(queue) > 0:
+            row, col = queue.pop(0)
+            if grid1[row][col] == 0:
+                isValid = False
+            for dir in dirs:
+                newRow = row + dir[0]
+                newCol = col + dir[1]
+                if 0 <= newRow < len(grid2) and 0 <= newCol < len(grid2[0]) and grid2[newRow][newCol] == 1:
+                    grid2[newRow][newCol] = 0
+                    queue.append([newRow, newCol])
+        return isValid, grid2
+
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
         candidates = self.init(grid2)
+        result = 0
 
         while len(candidates) > 0:
             row, col = candidates.pop(0)
             if grid2[row][col] == 0:
                 continue
-
-            grid2[row][col] = 0
-            queue = [[row, col]]
-            isValid = True
-            while len(queue) > 0:
-                row, col = queue.pop(0)
-                if grid1[row][col] == 0:
-                    isValid = False
-                for dir in dirs:
-                    newRow = row + dir[0]
-                    newCol = col + dir[1]
-                    if self.isValidPos(newRow, newCol, len(grid2), len(grid2[0])) and grid2[newRow][newCol] == 1:
-                        grid2[newRow][newCol] = 0
-                        queue.append([newRow, newCol])
+            isValid, grid2 = self.bfs(row, col, grid1, grid2)
             if isValid:
                 result += 1
         return result
-
-    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
-        return self.bfs(grid1, grid2)
         
