8ed0e06111dd921204d49b20dcd1715e4f34fcca
HyunJi / Rain <hyunjilim98@gmail.com>
2024-08-29 08:23:52 +0900

    Time: 2189 ms (47.06%), Space: 36.8 MB (57.37%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -1,36 +1,29 @@
-class Solution:
-    def init(self, grid: List[List[int]]):
-        candidates = []
-        for row in range(len(grid)):
-            for col in range(len(grid[row])):
-                if grid[row][col] == 1:
-                    candidates.append([row, col])
-        return candidates
+from collections import deque
 
+class Solution:
     def bfs(self, row: int, col: int, grid1: List[List[int]], grid2: List[List[int]]):
-        dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
         grid2[row][col] = 0
-        queue = [[row, col]]
+        queue = deque([(row, col)])
         isValid = True
         while len(queue) > 0:
-            row, col = queue.pop(0)
+            row, col = queue.popleft()
             if grid1[row][col] == 0:
                 isValid = False
-            for dir in dirs:
-                newRow = row + dir[0]
-                newCol = col + dir[1]
+            for dRow, dCol in dirs:
+                newRow, newCol = row + dRow, col + dCol
                 if 0 <= newRow < len(grid2) and 0 <= newCol < len(grid2[0]) and grid2[newRow][newCol] == 1:
                     grid2[newRow][newCol] = 0
-                    queue.append([newRow, newCol])
+                    queue.append((newRow, newCol))
         return isValid, grid2
 
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
-        candidates = self.init(grid2)
+        candidates = [[row, col] for row in range(len(grid2)) for col in range(len(grid2[row])) if grid2[row][col] == 1]
         result = 0
 
         while len(candidates) > 0:
-            row, col = candidates.pop(0)
+            row, col = candidates.pop()
             if grid2[row][col] == 0:
                 continue
             isValid, grid2 = self.bfs(row, col, grid1, grid2)
