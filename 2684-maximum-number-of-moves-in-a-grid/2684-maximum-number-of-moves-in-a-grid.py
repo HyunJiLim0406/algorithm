b2ae6e6a974bc9ed683f2ca6a54516c193a3ef20
HyunJi / Rain <hyunjilim98@gmail.com>
2024-10-30 08:01:31 +0900

    Time: 274 ms (40.58%), Space: 27.3 MB (66.67%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.py
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.py
@@ -1,21 +1,18 @@
 class Solution:
     def maxMoves(self, grid: List[List[int]]) -> int:
-        array = [[0] * len(grid[0]) for _ in range(len(grid))]
+        row, col = len(grid), len(grid[0])
+        dp = [[-1] * col] + [[0] * col for _ in range(row)] + [[-1] * col]
+        grid = [[1000001] * col] + grid + [[1000001] * col]
         result = 0
-        for i in range(1, len(grid[0])):
-            for j in range(len(grid)):
+        for i in range(1, col):
+            for j in range(1, row + 1):
                 flag = False
-                if j > 0 and array[j - 1][i - 1] != -1 and grid[j][i] > grid[j - 1][i - 1]:
-                    array[j][i] = max(array[j][i], array[j - 1][i - 1] + 1)
-                    flag = True
-                if array[j][i - 1] != -1 and grid[j][i] > grid[j][i - 1]:
-                    array[j][i] = max(array[j][i], array[j][i - 1] + 1)
-                    flag = True
-                if j + 1 < len(grid) and array[j + 1][i - 1] != -1 and grid[j][i] > grid[j + 1][i - 1]:
-                    array[j][i] = max(array[j][i], array[j + 1][i - 1] + 1)
-                    flag = True
+                for d in [-1, 0, 1]:
+                    if dp[j + d][i - 1] != -1 and grid[j][i] > grid[j + d][i - 1]:
+                        dp[j][i] = max(dp[j][i], dp[j + d][i - 1] + 1)
+                        flag = True
                 if not flag:
-                    array[j][i] = -1
-                result = max(result, array[j][i])
+                    dp[j][i] = -1
+                result = max(result, dp[j][i])
         return result
         
