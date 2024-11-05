ffb98ce7ab3e84c9ddc93094e6352158ccebaae3
HyunJi / Rain <hyunjilim98@gmail.com>
2024-11-05 16:17:56 +0900

    Time: 124 ms (92.41%), Space: 19.2 MB (82.46%) - LeetHub

---
+++ 3163-string-compression-iii/3163-string-compression-iii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def compressedString(self, word: str) -> str:
+        result, cnt, cur = '', 1, word[0]
+        for w in word[1:]:
+            if w == cur:
+                if cnt < 9:
+                    cnt += 1
+                else:
+                    result += (str(cnt) + cur)
+                    cnt = 1
+            else:
+                result += (str(cnt) + cur)
+                cnt, cur = 1, w
+        result += (str(cnt) + cur)
+        return result
+        
